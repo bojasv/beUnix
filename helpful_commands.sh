8c3339d7b7bd67e107955ff6f3a2533b35c44523
bojasv <59419054+bojasv@users.noreply.github.com>
2023-10-19 08:40:53 +0530

    Create helpful_commands.sh

---
+++ helpful_commands.sh
@@ -0,0 +1,6 @@
+# comments
+# `$` - represents the exit status of the previous command. Exit status as `0` if successful, else `1` for unsuccessful.
+# commonly used commands
+# working with files and directories
+# managing data
+# file I/O handling
